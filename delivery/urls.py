e57237203259c35f090cc21600f484ef59b87d2b
zombie828 <81745002+zombie828@users.noreply.github.com>
2022-09-06 18:11:50 +0900

    Add files via upload

---
+++ delivery/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from delivery import views
+
+urlpatterns = [
+    path('orders/', views.order_list, name="order_list"),
+    # path('menus/<int:shop>', views.menu, name="menu"),
+    # path('order/', views.order, name="order"),
+]
---
+++ delivery/views.py
@@ -0,0 +1,20 @@
+import re
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from order.models import Shop,Menu,Order,Orderfood
+from order.serializers import ShopSerializer,MenuSerializer
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+@csrf_exempt 
+def order_list(request):
+    if request.method == 'GET':  
+        order_list = Order.objects.all()
+        return render(request, 'delivery/order_list.html', {'order_list':order_list})
+
+
+    elif request.method == 'POST':  
+        order_item = Order.objects.get(pk=int(request.POST['order_id']))
+        order_item.deliver_time = 1
+        order_item.save()
+        return render(request, 'delivery/success.html')
